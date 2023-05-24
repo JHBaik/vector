bc838eba82b9691351adccb44af8e36605d0ae70
JHBaik <contact@bjh.works>
2023-05-25 04:02:10 +0900

    create obj , selection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,212 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+
+import styles from './page.module.css';
+import React, { useEffect, useMemo, useRef, useState, WheelEvent } from 'react';
+import * as paper from 'paper';
+import { PaperScope } from 'paper';
+
+function Canvas({
+                  pScope,
+                  ...props
+                }: {
+  pScope: paper.PaperScope,
+} & React.CanvasHTMLAttributes<HTMLCanvasElement>) {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  console.log('canvas render');
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    console.log({x: pScope});
+    console.log('setup pp');
+    pScope.setup(canvas);
+
+  }, [pScope, canvasRef]);
+
+  return (
+    <canvas ref={canvasRef} {...props}>
+    </canvas>
+  );
+}
 
 export default function Home() {
+  const paper = useMemo(() => new PaperScope(), []);
+
+  const tCursor = useMemo(() => new paper.Tool(), [paper]);
+  const tCircle = useMemo(() => new paper.Tool(), [paper]);
+  const tRectangle = useMemo(() => new paper.Tool(), [paper]);
+  const tLine = useMemo(() => new paper.Tool(), [paper]);
+
+  tCursor.activate();
+
+  const [strokeColor, setStrokeColor] = useState(new paper.Color(0, 0, 0) as paper.Color);
+  const [fillColor, setFillColor] = useState(new paper.Color(0, 0, 0) as paper.Color);
+
+  function activateCursor() {
+    tCursor.activate();
+    const keyStat = {
+      meta: false
+    };
+
+    tCursor.onMouseUp = (e: paper.ToolEvent) => {
+      let selectedItem = e.item;
+
+      if (!keyStat.meta) {
+        paper.project.deselectAll();
+      }
+      if (selectedItem) {
+        selectedItem.selected = true;
+      }
+    };
+
+    tCursor.onKeyDown = (e: paper.KeyEvent) => {
+      if (e.key === 'meta') {
+        keyStat.meta = true;
+      }
+    };
+
+    tCursor.onKeyUp = (e: paper.KeyEvent) => {
+      if (e.key === 'meta') {
+        keyStat.meta = false;
+      }
+    };
+  }
+
+  function addCircle() {
+    tCircle.activate();
+
+    function emptyCircle() {
+      const circle = new paper.Shape.Circle([0, 0], 0);
+      circle.strokeColor = strokeColor;
+      circle.fillColor = fillColor;
+      return circle;
+    }
+
+    let circle = emptyCircle();
+
+    tCircle.onMouseDown = (e: paper.ToolEvent) => {
+      circle.bounds.center = e.downPoint;
+    };
+
+    tCircle.onMouseDrag = (e: paper.ToolEvent) => {
+      circle.radius = e.downPoint.subtract(e.point).abs().length;
+    };
+
+    tCircle.onMouseUp = () => {
+      if (circle.radius !== 0) {
+        circle = emptyCircle();
+      }
+    };
+  }
+
+  function addRectangle() {
+    tRectangle.activate();
+
+    let rect = new paper.Shape.Rectangle([0, 0, 0, 0]);
+    rect.strokeColor = strokeColor;
+    rect.fillColor = fillColor;
+
+    tRectangle.onMouseDown = (e: paper.ToolEvent) => {
+      rect.bounds.topLeft = e.downPoint;
+    };
+
+    tRectangle.onMouseDrag = (e: paper.ToolEvent) => {
+      let delta = e.downPoint.subtract(e.point);
+      rect.size = new paper.Size(delta.abs());
+      switch (delta.quadrant) {
+        case 1:
+          rect.bounds.bottomRight = e.downPoint;
+          break;
+        case 2:
+          rect.bounds.bottomLeft = e.downPoint;
+          break;
+        case 3:
+          rect.bounds.topLeft = e.downPoint;
+          break;
+        case 4:
+          rect.bounds.topRight = e.downPoint;
+          break;
+      }
+    };
+
+    tRectangle.onMouseUp = () => {
+      rect = new paper.Shape.Rectangle([0, 0, 0, 0]);
+      rect.strokeColor = strokeColor;
+      rect.fillColor = fillColor;
+    };
+  }
+
+  function addLine() {
+    tLine.activate();
+    let line: paper.Path = new paper.Path();
+    line.strokeColor = new paper.Color(255, 0, 0);
+
+    tLine.onMouseDown = (e: paper.ToolEvent) => {
+      line.moveTo(e.point);
+    };
+
+    tLine.onMouseDrag = (e: paper.ToolEvent) => {
+      line.lineTo(e.point);
+    };
+
+    tLine.onMouseUp = (e: paper.ToolEvent) => {
+      line = new paper.Path();
+      line.strokeColor = new paper.Color(255, 0, 0);
+    };
+  }
+
+  function onMouseWheel(event: WheelEvent<HTMLCanvasElement>) {
+    var newZoom = paper.view.zoom;
+    var oldZoom = paper.view.zoom;
+    if (event.deltaY > 0) {
+      console.log('zoom -');
+      newZoom = paper.view.zoom * 0.98;
+    } else {
+      console.log('zoom +');
+      newZoom = paper.view.zoom * 1.02;
+    }
+
+    var beta = oldZoom / newZoom;
+    const {x, y} = (event.target as HTMLCanvasElement).getBoundingClientRect();
+    var mousePosition = (new paper.Point(event.clientX, event.clientY)).subtract([x, y]);
+
+    //viewToProject: gives the coordinates in the Project space from the Screen Coordinates
+    var viewPosition = paper.view.viewToProject(mousePosition);
+
+    var mpos = viewPosition;
+    var ctr = paper.view.center;
+
+    var pc = mpos.subtract(ctr);
+    var offset = mpos.subtract(pc.multiply(beta)).subtract(ctr);
+
+    paper.view.zoom = newZoom;
+    paper.view.center = paper.view.center.add(offset);
+
+    // event.preventDefault();
+  }
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <div>
+        <button onClick={() => activateCursor()}>Selection</button>
+        <span>Add obj</span>
+        <ul>
+          <li>
+            <button onClick={() => addCircle()}>Circle</button>
+          </li>
+          <li>
+            <button onClick={() => addRectangle()}>Rectangle</button>
+          </li>
+          <li>
+            <button onClick={() => addLine()}>Line</button>
+          </li>
+        </ul>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <Canvas
+        pScope={paper}
+        height={600} width={900} style={{background: 'white'}}
+        onWheel={onMouseWheel}
+      ></Canvas>
     </main>
-  )
+  );
 }
